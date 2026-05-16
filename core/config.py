1d13b378d5c142ac64864ec9c9e6db6bfb85dc08
Maryanne <maryannebalch0@gmail.com>
2026-05-16 00:13:29 -0500

    Change S3 bucket to use environment variable
    Updated the S3 bucket configuration to use an environment variable.

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ import boto3
 # AWS clients
 s3      = boto3.client("s3", region_name="us-east-1")
 bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
-BUCKET   = "tpmb-resume-training-data"
+BUCKET   = os.environ.get("RESUME_S3_BUCKET")
 MODEL_ID = "us.amazon.nova-pro-v1:0"
 
 # paths
